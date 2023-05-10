f9c1dede34e385bf64cfdac5dae8c4a626952109
nazo6 <git@nazo6.dev>
2023-05-11 00:35:50 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,62 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'IP Check',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
       home: const HomePage(title: 'IP Checker'),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
   final String title;
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  List<Future<ProviderResult>> providerResults = [];
+
+  @override
+  void initState() {
+    for (var provider in providers) {
+      providerResults.add(provider.getData());
+    }
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView.builder(
-          itemCount: providers.length,
-          itemBuilder: (context, index) {
-            return ProviderCard(provider: providers[index]);
-          },
-        ));
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: providers.length,
+        itemBuilder: (context, index) {
+          return ProviderCard(
+              providerResultFuture: providerResults[index],
+              providerName: providers[index].name);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _reload();
+        },
+        child: const Icon(Icons.replay),
+      ),
+    );
+  }
+
+  void _reload() {
+    var newProviderResults = <Future<ProviderResult>>[];
+    for (var provider in providers) {
+      newProviderResults.add(provider.getData());
+    }
+    setState(() {
+      providerResults = newProviderResults;
+    });
   }
 }
--- lib/provider_card.dart
+++ lib/provider_card.dart
@@ -2,56 +2,56 @@ import 'package:flutter/material.dart';
 
 import 'providers/provider.dart';
 
-class ProviderCard extends StatefulWidget {
-  final Provider provider;
+class ProviderCard extends StatelessWidget {
+  final Future<ProviderResult> providerResultFuture;
+  final String providerName;
 
   const ProviderCard({
     Key? key,
-    required this.provider,
+    required this.providerResultFuture,
+    required this.providerName,
   }) : super(key: key);
-
-  @override
-  ProviderCardState createState() => ProviderCardState();
-}
-
-class ProviderCardState extends State<ProviderCard> {
-  late Future<ProviderResult> _futureProviderResult;
-
-  @override
-  void initState() {
-    super.initState();
-    _futureProviderResult = widget.provider.getData();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
         child: FutureBuilder(
-      future: _futureProviderResult,
+      future: providerResultFuture,
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          ProviderResult result = snapshot.data as ProviderResult;
-          return ListTile(
-            title: Text(widget.provider.name),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(result.ip),
-                Text(result.location ?? 'Unknown location'),
-                Text(result.isp ?? 'Unknown ISP'),
-              ],
-            ),
-          );
-        } else if (snapshot.hasError) {
-          return ListTile(
-            title: Text(widget.provider.name),
-            subtitle: Text('Error: ${snapshot.error}'),
-          );
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            ProviderResult result = snapshot.data as ProviderResult;
+            List<Widget> children = [];
+            children.add(Text("ip: ${result.ip}"));
+            if (result.location != null) {
+              children.add(Text("location: ${result.location}"));
+            }
+            if (result.isp != null) {
+              children.add(Text("ISP: ${result.isp}"));
+            }
+
+            return ListTile(
+              title: Text(providerName),
+              subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: children),
+            );
+          } else if (snapshot.hasError) {
+            return ListTile(
+              title: Text(providerName),
+              subtitle: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return ListTile(
+                title: Text(providerName), subtitle: const Text('Logic error'));
+          }
         } else {
-          return ListTile(
-            title: Text(widget.provider.name),
-            subtitle: const Text('Loading...'),
-          );
+          return const Center(
+              child: SizedBox(
+            height: 50,
+            width: 50,
+            child: CircularProgressIndicator(),
+          ));
         }
       },
     ));
